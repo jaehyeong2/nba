0c9af49b5a5d36f4769e11109e789a9ab4efb124
jae <wogud1514@naver.com>
2022-07-27 21:41:58 +0900

    시작

---
+++ src/main/java/jjfactory/nbaplayersearching/NbaPlayerSearchingApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.nbaplayersearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaPlayerSearchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaPlayerSearchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjfactory/nbaplayersearching/NbaPlayerSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.nbaplayersearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaPlayerSearchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
